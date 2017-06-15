2756e0a68a091f621f486637b77a3d188fefb225
Jeremy Walsh <jeremy_walsh@live.com>
2017-06-15 16:57:14 +0000

    Page class: protocol property added

--- system/classes/Page.rb
+++ system/classes/Page.rb
@@ -1,15 +1,18 @@
+require 'net/http'
 class Page
   def initialize(url)
     url_sections = url.split("/")
+    @protocol = "";
     @base_url = ""
-    @base_url = url_sections[0] + "//" + url_sections[2]
     @path = ""
+    @protocol = url_sections[0]
+    @base_url = url_sections[2]
     url_sections.drop(3).each do |section|
       @path.concat("/" + section)
     end
   end
   
   def to_s
-   "Page(base_url:" + @base_url.to_s + ",path:" + @path.to_s + ",html:" + @html.to_s + ")"
+   "Page(protocol: " + @protocol + ",base_url:" + @base_url.to_s + ",path:" + @path.to_s + ",html:" + @html.to_s + ")"
   end
 end
