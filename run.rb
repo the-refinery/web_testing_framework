196110b2b55d854438beec3bb4f2f877ff88f06f
Jeremy Walsh <jeremy_walsh@live.com>
2017-06-15 16:19:30 +0000

    page base_url and path variable algorithms, testing text on run page

--- run.rb
+++ run.rb
@@ -1,1 +1,4 @@
 require_relative 'system/classes/Page.rb'
+
+page = Page.new("http://oec.staging.t-r.io/solutions/d2d-express")
+puts page.to_s
--- system/classes/Page.rb
+++ system/classes/Page.rb
@@ -1,10 +1,15 @@
 class Page
   def initialize(url)
-    @base_url = "base"
-    @path = "path"
-    @html="html"
+    url_sections = url.split("/")
+    @base_url = ""
+    @base_url = url_sections[0] + "//" + url_sections[2]
+    @path = ""
+    url_sections.drop(3).each do |section|
+      @path = @path.to_s + "/" + section.to_s
+    end
   end
+  
   def to_s
-   "Page(base_url:" + @base_url + ",path:" + @path + ",html:" + @html + ")"
+   "Page(base_url:" + @base_url.to_s + ",path:" + @path.to_s + ",html:" + @html.to_s + ")"
   end
 end
