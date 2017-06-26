dbbb4fcc9a7bce36e7bb0a6bcb118ecaca93bb26
Jeremy Walsh <jeremy_walsh@live.com>
2017-06-26 21:12:46 +0000

    Site Class initiation

--- run.rb
+++ run.rb
@@ -1,7 +1,14 @@
 require 'open-uri'
 require 'nokogiri'
+require_relative 'system/classes/Hyperlink.rb'
+
+class Site
+  def initialize(domain)
+    @domain = domain
+    @hyperlinks = []
+  end
+end
 
 site = 'http://oec.staging.t-r.io'
 html = open(site).read
 document = Nokogiri::HTML(html)
-puts document.to_s
