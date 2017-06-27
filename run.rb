4a9e1329394a897fd9cdbdc3c07d96bf3fc90541
Jeremy Walsh <jeremy_walsh@live.com>
2017-06-27 17:42:06 +0000

    Site hyperlink extraction for entire subdomain

--- run.rb
+++ run.rb
@@ -21,38 +21,60 @@ class Site
     puts ""
     puts @domain.cyan
     get_new_links @domain
+    puts @hyperlinks.length.to_s.green
+    puts ""
+    while true
+      i = 0
+      while i < @hyperlinks.length
+        if (@hyperlinks[i].href.start_with?(@domain) && !@links_analyzed[i])
+          puts @hyperlinks[i].href.cyan
+          get_new_links @hyperlinks[i].href
+          puts i.to_s + " " + @hyperlinks.length.to_s.green
+          puts ""
+          break
+        end
+        i = i + 1
+      end
+      break if i == @hyperlinks.length - 1
+    end
   end
   
   private
   
   def get_new_links site_page
-    @agent.get(site_page)
-    links = @agent.page.search('a')
-    links.each do |link|
-      href = link.attr('href')
-      href = @domain + href if href[0] == '/'
-      new_link = Hyperlink.new href, link.attr('target').to_s
-      if (!contains_hyperlink? new_link)
-        @hyperlinks.push new_link
-        @links_analyzed.push false
+    begin
+      @agent.get(site_page)
+      links = @agent.page.search('a')
+      links.each do |link|
+        href = link.attr('href')
+        href = @domain + href if href[0] == '/'
+        new_link = Hyperlink.new href, link.attr('target').to_s
+        if (!contains_hyperlink? new_link)
+          @hyperlinks.push new_link
+          @links_analyzed.push false
+        end
       end
-    end
-    sort_hyperlinks
-    check_as_analyzed site_page
-    @hyperlinks.length.times do |i|
-      print @hyperlinks[i].href + " -- "
-      if (@links_analyzed[i])
-        print @links_analyzed[i].to_s.green
-      else
-        print @links_analyzed[i].to_s.red
-      end
-      print "\n"
+      sort_hyperlinks
+      check_as_analyzed site_page
+      rescue Exception => e
+        check_as_analyzed site_page
+      ensure
+        @hyperlinks.length.times do |i|
+          print @hyperlinks[i].href + " -- "
+          if (@links_analyzed[i])
+            print @links_analyzed[i].to_s.green
+          else
+            print @links_analyzed[i].to_s.red
+          end
+          print "\n"
+        end
     end
   end
   
   def contains_hyperlink? link
     @hyperlinks.each do |hyperlink|
-      return true if link.equal? hyperlink
+      #return true if link.equal? hyperlink
+      return true if link.href == hyperlink.href
     end
     false
   end
